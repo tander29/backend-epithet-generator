d7bfc007f67e1319c84d22fd30d2e926d516bfa0
Travis Anderson <thetanderson@gmail.com>
2019-02-08 09:15:21 -0500

    added ability to use debugger

--- submissions/sprint_b/app.py
+++ submissions/sprint_b/app.py
@@ -1,14 +1,15 @@
 from sprint_b import app
 from sprint_b.helpers import EpithetGenerator as Epgen
 from sprint_b.helpers import Vocabulary as Vocab
-from pathlib import PurePath as Path
 import flask
 import os
 
-p = Path(os.getcwd())
-project_root = str(p.parent.parent)
-json_path = os.path.join(project_root+'/resources', 'data.json')
-file_path = os.path.join(project_root+'/resources', 'data.csv')
+try:
+    ROOT_PATH = os.environ["EPITHET_ROOT"]
+except KeyError:
+    ROOT_PATH = '../../'
+resourse_path = os.path.join(ROOT_PATH, 'resources/')
+json_path = resourse_path + 'data.json'
 
 
 @app.route('/')
